d9cbeb0e21c49e565c958234dd5b0824f4fe7f75
eliakimceleste <eliakimceleste@gmail.com>
2024-07-23 10:35:51 +0100

    envoi du fichier main.py

--- server-fastapi/main.py
+++ server-fastapi/main.py
@@ -1,9 +1,13 @@
 from typing import Union
-
+from api.services.post_generator_service import PostGeneratorService
 from api.controllers.post_controller import PostGenerator
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
+generator = PostGeneratorService()
 
 
 # def runner():
@@ -12,11 +16,22 @@ app = FastAPI()
 #     app.include_router(hello.router)
 
 
-@app.post("/generate-post")
+
+@app.get("/generate-post")
 def read_root():
-    return {"Hello": "World"}
+   return 
+
+@app.get("/get_files/{id}")
+def view_image(id):
+    name = [file.split(".")[0] for file in os.listdir(os.path.join(os.getcwd(),'static'))]
+    image_path = os.path.join(os.getcwd(),'static', f'{id}.png')
+    if image_path.split("/")[-1].split(".")[0] in name:
+        return FileResponse(image_path)
+    print(image_path.split("/")[-1])
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
